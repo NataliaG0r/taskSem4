6718a0ae0ccb341ae498e14c3ddecc994f08dcdf
Natalia_Go <gorshana@gmail.com>
2023-02-15 18:09:51 +0300

    2 task

--- Program.cs
+++ Program.cs
@@ -5,21 +5,21 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-System.Console.WriteLine("введите первое число(основание)");
-double a =Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("введите второе число(степень)");
-double b =Convert.ToInt32(Console.ReadLine());
-if ((b>0)&&( b % 1 == 0))
-{
-   double result =1;
-   for (int i = 1; i <= b; i++)
-     {
-        result =result*a;
-}
-System.Console.WriteLine(result);
-}
+// System.Console.WriteLine("введите первое число(основание)");
+// int a =Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("введите второе число(степень)");
+// int b =Convert.ToInt32(Console.ReadLine());
+// if ((b>0)&&( b % 1 == 0))
+// {
+//    int result =1;
+//    for (int i = 1; i <= b; i++)
+//      {
+//         result =result*a;
+// }
+// System.Console.WriteLine(result);
+// }
 
-else System.Console.WriteLine($"{b}не является натуральным числом");
+// else System.Console.WriteLine($"{b}не является натуральным числом");
 
 
 
@@ -32,6 +32,16 @@ else System.Console.WriteLine($"{b}не является натуральным 
 
 // 9012 -> 12
 
+System.Console.WriteLine("введите  число");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (num / 10 != 0)
+{
+    sum = sum + num % 10;
+    num = num / 10;
+}
+System.Console.WriteLine(sum+num);
 
 
 
